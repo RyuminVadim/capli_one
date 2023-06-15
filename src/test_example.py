2440da5a7707c0d5d4595ccf946a4ee8930997c0
RyuminVadim <dflbr201@mail.ru>
2023-06-15 11:52:44 +0700

    Corect test

--- src/test_example.py
+++ src/test_example.py
@@ -3,12 +3,10 @@ import main
 def test_area_GaussianBlur():
     file = 'test.png'
     _, area = main.processing(file,0)
-    #102900
     assert   10745.0 - (10745.0*0.01)<= area<= 10745.0 + (10745.0*0.01)
 
 def test_area_adaptiveThreshold():
     file = 'test.png'
     _, area = main.processing(file,1)
-    #102900
     assert   10745.0 - (10745.0*0.01)<= area<= 10745.0 + (10745.0*0.01)
 
